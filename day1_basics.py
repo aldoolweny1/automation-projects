69192e9e05d8bcae94221743b34a82d4d792e04e
aldoolweny1 <aldoolweny1@gmail.com>
2026-01-01 01:04:28 -0800

    Day 1:Learned what python, scripts, functions, and variables. Worked on VSCode and printed out hello world and many more.

---
+++ day1_basics.py
@@ -0,0 +1,7 @@
+name = "Aldo"
+age = 23
+goal = "IT Automation with Python"
+
+print("Name:", name)
+print("Age:", age)
+print("Goal:", goal)
---
+++ sum.py
@@ -0,0 +1,5 @@
+Python 3.13.9 (tags/v3.13.9:8183fa5, Oct 14 2025, 14:09:13) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> num1 = 1
+>>> num2 = 2
+>>> sum = num1 + num2
